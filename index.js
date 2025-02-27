edc11329e592dc90435aaf45a8f787f093964225
Satyam Kumar <subham11@gmail.com>
2025-02-28 02:54:49 +0530

    added to reflect animated value

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import 'react-native-reanimated'; // ✅ Required for Reanimated to work properly
+import 'react-native-gesture-handler'; // ✅ Needed for gesture navigation
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
